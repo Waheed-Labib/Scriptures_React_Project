9c3af482f8f1c5cea117e29353163229692e3655
Waheed-Labib <labibwahid28@gmail.com>
2024-09-14 13:37:50 +0600

    updated pagination in surah content

--- src/pages/surah/surah-content/surah-content-pagination/verses-page-navigation/VersesPageNavigation.jsx
+++ src/pages/surah/surah-content/surah-content-pagination/verses-page-navigation/VersesPageNavigation.jsx
@@ -1,7 +1,19 @@
+import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
+
 const VersesPageNavigation = () => {
     return (
-        <div>
-            Verses Page Navigation
+        <div className="flex items-center gap-2 text-slate-700">
+
+            <div className="text-2xl hover:text-slate-800">
+                <FaChevronCircleLeft></FaChevronCircleLeft>
+            </div>
+
+            <p>Page 1</p>
+
+            <div className="text-2xl hover:text-slate-800">
+                <FaChevronCircleRight></FaChevronCircleRight>
+            </div>
+
         </div>
     );
 };
--- src/pages/surah/surah-content/surah-content-pagination/verses-per-page/VersesPerPage.jsx
+++ src/pages/surah/surah-content/surah-content-pagination/verses-per-page/VersesPerPage.jsx
@@ -1,6 +1,6 @@
 const VersesPerPage = () => {
     return (
-        <div className="text-xs">
+        <div className="text-xs text-slate-600">
             Per Page : <span className="border border-slate-600 rounded-sm p-1">10 Verses</span>
         </div>
     );
