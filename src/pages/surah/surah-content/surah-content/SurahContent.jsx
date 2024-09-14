dbe49d0022e8361bfd54ecf8534663173b7b1935
Waheed-Labib <labibwahid28@gmail.com>
2024-09-14 20:13:53 +0600

    scroll to the surahContent component's top when page changes

--- src/pages/surah/surah-content/surah-content/SurahContent.jsx
+++ src/pages/surah/surah-content/surah-content/SurahContent.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ErrorComponent from "../../../../components/error-component/ErrorComponent";
 import SimpleSkeleton from "../../../../components/simple-skeleton/SimpleSkeleton";
 import { useVersesInfo } from "../../../../hooks/useVersesInfo";
@@ -10,7 +10,17 @@ import VerseComponent from "../verse-component/verse-component/VerseComponent";
 
 const SurahContent = ({ arabicFont }) => {
 
+    const scrollComponentRef = useRef(null);
+
+    const scrollComponent = () => {
+        scrollComponentRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+
     const [page, setPage] = useState(1)
+
+    useEffect(() => {
+        if (page) scrollComponent()
+    }, [page])
 
     const { state } = useVersesInfo(page);
     const { versesInfo, loading, error } = state;
@@ -19,37 +29,40 @@ const SurahContent = ({ arabicFont }) => {
     return (
         <div className="p-2 bg-slate-100">
             <div className="h-[80vh] overflow-y-scroll">
-                <SurahStarting></SurahStarting>
-                <div className="border rounded p-8 pt-0 bg-slate-200">
-                    {
-                        loading && <div className="flex flex-col gap-2">
-                            {
-                                Array.from({ length: 10 }).map((_, index) => <SimpleSkeleton key={index}></SimpleSkeleton>)
-                            }
-                        </div>
-                    }
-                    {
-                        error && <ErrorComponent
-                            errorType={'Verses Fetching Failed'}
-                            errorText={error}
-                        ></ErrorComponent>
-                    }
-                    {
-                        !loading && !error && <>
-                            {
-                                versesData?.map((verseData, index) => <VerseComponent
-                                    key={index}
-                                    page={page}
-                                    verseData={verseData}
-                                    arabicFont={arabicFont}
-                                ></VerseComponent>)
-                            }
-                        </>
-                    }
-                    <SurahContentPagination
-                        page={page}
-                        setPage={setPage}
-                    ></SurahContentPagination>
+                <div ref={scrollComponentRef}>
+                    <SurahStarting></SurahStarting>
+                    <div className="border rounded p-8 pt-0 bg-slate-200">
+                        {
+                            loading && <div className="flex flex-col gap-2">
+                                {
+                                    Array.from({ length: 10 }).map((_, index) => <SimpleSkeleton key={index}></SimpleSkeleton>)
+                                }
+                            </div>
+                        }
+                        {
+                            error && <ErrorComponent
+                                errorType={'Verses Fetching Failed'}
+                                errorText={error}
+                            ></ErrorComponent>
+                        }
+                        {
+                            !loading && !error && <>
+                                {
+                                    versesData?.map((verseData, index) => <VerseComponent
+                                        key={index}
+                                        page={page}
+                                        verseData={verseData}
+                                        arabicFont={arabicFont}
+                                    ></VerseComponent>)
+                                }
+                            </>
+                        }
+
+                        <SurahContentPagination
+                            page={page}
+                            setPage={setPage}
+                        ></SurahContentPagination>
+                    </div>
                 </div>
             </div>
         </div>
