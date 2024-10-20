97d3a0fb46e36c2203d4d793e7b2b04c6c6a9b9e
Waheed-Labib <labibwahid28@gmail.com>
2024-10-20 18:41:54 +0600

    updated commonSearch.js

--- src/utilities/search-methods/commonSearch.js
+++ src/utilities/search-methods/commonSearch.js
@@ -1,3 +1,15 @@
 export const commonSearch = (searchText, setSearchResults) => {
-    setSearchResults(searchText)
+    // check if it is a surah number
+    if (!isNaN(searchText)) {
+        const surahNum = parseInt(searchText)
+        if (surahNum < 1 || surahNum > 114) {
+            setSearchResults([
+                {
+                    searchType: 'invalid'
+                }
+            ])
+
+            return
+        }
+    }
 }
