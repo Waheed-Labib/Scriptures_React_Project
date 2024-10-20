0f54c8051bd2695ce5f5d350115b148c9dff8556
Waheed-Labib <labibwahid28@gmail.com>
2024-10-20 12:58:58 +0600

    updated useSearchResult and CommonSearchBox

--- src/components/common-search-box/CommonSearchBox.jsx
+++ src/components/common-search-box/CommonSearchBox.jsx
@@ -1,19 +1,23 @@
 /* eslint-disable react/prop-types */
 import { FaSearch } from "react-icons/fa";
-import { commonSearch } from "../../utilities/search-methods/common-search/commonSearch";
+import useSearchResult from "../../hooks/common-search-hooks/useSearchResult";
+import { useState } from "react";
 
 const CommonSearchBox = ({ setSearchResults }) => {
 
-    const handleSearchInputChange = e => {
-        const searchText = e.target.value;
-        const result = commonSearch(searchText);
-        setSearchResults(result);
-    }
+    const [searchText, setSearchText] = useState('');
+
+    const result = useSearchResult(searchText);
+    console.log(result)
 
     return (
         <div>
             <div>
-                <input onChange={handleSearchInputChange} placeholder="Search Quran" className="w-full h-10 rounded ring-1 ring-inset ring-cyan-950 placeholder:text-gray-600 pl-4"></input>
+                <input
+                    onChange={e => setSearchText(e.target.value)}
+                    placeholder="Search Quran"
+                    className="w-full h-10 rounded ring-1 ring-inset ring-cyan-950 placeholder:text-gray-600 pl-4"
+                ></input>
             </div>
 
             <div className="pl-1 text-xs text-cyan-950 flex items-center gap-2 mt-2">
