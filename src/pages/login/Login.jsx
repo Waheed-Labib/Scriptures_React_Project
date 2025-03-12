36e5c026774d3f90c43f2221f5d72495c29c9fc3
Waheed-Labib <labibwahid28@gmail.com>
2025-03-12 17:38:27 +0600

    updated login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -10,8 +10,45 @@ import {
     Label,
 } from 'keep-react'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { server } from '../../constants';
+import axios from 'axios';
+import SuccessAlert from '../../components/success-alert/SuccessAlert';
+import ErrorAlert from '../../components/error-alert/ErrorAlert';
 
 const Login = () => {
+
+    const [loading, setLoading] = useState(false);
+
+    const [successMsg, setSuccessMsg] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        setLoading(true)
+
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        axios.post(`${server}/users/login`, {
+            email,
+            password
+        })
+            .then(function (response) {
+                setSuccessMsg(`Welcome Back, ${response.data.data.user.fullName}!`)
+                event.target.reset();
+            })
+            .catch(function (error) {
+                const match = error.response.data.match(/<pre>Error: (.*?)<br>/);
+                const errorMessage = match ? match[1] : "Unknown error";
+
+                setErrorMsg(`Failed! ${errorMessage}`)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }
+
     return (
         <div className='w-full'>
             <Card className="max-w-sm mx-auto my-12">
@@ -19,7 +56,7 @@ const Login = () => {
                     <CardHeader>
                         <CardTitle className='text-gray-700'>Login</CardTitle>
                     </CardHeader>
-                    <form className="space-y-2">
+                    <form onSubmit={handleLogin} className="space-y-2">
                         <fieldset className="space-y-1">
                             <Label htmlFor="email">Email*</Label>
                             <div className="relative">
@@ -38,13 +75,35 @@ const Login = () => {
                                 </InputIcon>
                             </div>
                         </fieldset>
-                        <Button type="submit" className="!mt-3 block w-full bg-cyan-700 hover:bg-slate-700" >
-                            Login
-                        </Button>
+                        {
+                            loading ?
+                                <Button className="!mt-3 block w-full bg-cyan-700 hover:bg-slate-700" >
+                                    Loading ...
+                                </Button>
+                                :
+                                <Button type="submit" className="!mt-3 block w-full bg-cyan-700 hover:bg-slate-700" >
+                                    Login
+                                </Button>
+                        }
+
                     </form>
                     <p className='text-center mt-8 text-xs'>Don&apos;t have an account? <Link to='/enroll' className='text-sm text-cyan-600 font-medium hover:underline transition'>Enroll Now</Link></p>
                 </CardContent>
             </Card>
+
+            {
+                successMsg && <SuccessAlert
+                    success={successMsg}
+                    setSuccess={setSuccessMsg}
+                ></SuccessAlert>
+            }
+
+            {
+                errorMsg && <ErrorAlert
+                    error={errorMsg}
+                    setError={setErrorMsg}
+                ></ErrorAlert>
+            }
         </div>
     )
 }
